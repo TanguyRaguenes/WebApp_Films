6956c01ae7dde6cbe7cbaa8fe3d7e300f36d41be
TanguyRaguenes <161499054+TanguyRaguenes@users.noreply.github.com>
2024-03-24 18:53:50 +0100

    Update 24-03-2024

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 const  titreFilm = document.querySelector('#titreFilm');
 const btn_rechercher = document.getElementById('btn_rechercher');
-const btn_acteur_suivant = document.getElementById('btn_acteur_suivant');
-const btn_acteur_precedent = document.getElementById('btn_acteur_precedent');
-const boutonFilmographieSuivant = document.getElementById('boutonFilmographieSuivant');
-const boutonRetour = document.getElementById('boutonRetour');
+const btn_acteurs_suivants = document.getElementById('btn_acteurs_suivants');
+const btn_acteurs_precedents = document.getElementById('btn_acteurs_precedents');
+
+const btn_retour = document.getElementById('btn_retour');
+const btn_films_precedents = document.getElementById('btn_films_precedents');
+const btn_films_suivants = document.getElementById('btn_films_suivants');
+
+
+
 const photoActeurs = document.querySelectorAll('.photoActeurs');
-const filmsActeur = document.getElementById('infosActeur');
+const photoFilmographie = document.querySelectorAll('.photoFilmographie');
+
+const ctner_infos_acteur = document.getElementById('ctner_infos_acteur');
 
 let casting=[];
 let filmographieActeur=[];
@@ -17,15 +24,119 @@ let idFilm = 385687;
 
 let titresFilms =[];
 
-// -------------------------------------------------------------------------
+
+// FILMOGRAPHIE'______________________________________________________________________________________________________'
+
+
+btn_films_suivants.addEventListener("click", ()=>{
+
+    idFilmographie+=5
+    ctrlIdFilmographie()
+    afficherFilmographie();
+    
+});
+
+btn_films_precedents.addEventListener("click", ()=>{
+
+    idFilmographie-=5
+    ctrlIdFilmographie()
+    afficherFilmographie();
+    
+});
+
+// btn_acteurs_suivants.addEventListener('click',()=>{
+//     idCasting+=5;
+    
+//     ctrlIdCasting()
+//     afficherActeur(idCasting);
+// });
+
+// btn_acteurs_precedents.addEventListener('click',()=>{
+//     idCasting-=5;
+
+//     ctrlIdCasting()
+//     afficherActeur(idCasting);
+
+// });
+
+function ctrlIdFilmographie(){
+    if(idFilmographie+5>filmographieActeur.length){
+        idFilmographie=filmographieActeur.length-6;
+    }else if(idFilmographie<0){
+        idFilmographie=0;
+    }
+}
+
+
+photoActeurs.forEach((e)=>{
+    e.addEventListener('click', ()=>{
+        const idActeur = Number(e.id.substring(e.id.length-1))+idCasting;
+        requeterInfosActeur(casting[idActeur].id);
+    })
+})
+
+function requeterInfosActeur(id){
+    idFilmographie=0;
+    fetch('https://api.themoviedb.org/3/person/' + id + '/movie_credits?api_key=b642a2df5b6d3048d4f07cd2a377518c&language=en-US')
+    .then((response)=>{
+        response.json()
+        .then((json)=>{
+
+            filmographieActeur=json.cast;
+            console.log(filmographieActeur);
+            filmographieActeur.sort(function trie(a,b){
+
+                if (a.release_date ==""){
+                    date1="1000-01-01"
+                }else{
+                    date1 = new Date(a.release_date)
+                }
+
+                if (b.release_date == ""){
+                    date2="1000-01-01"
+                }else{
+                    date2 = new Date(b.release_date)
+                }
+
+                return date2 - date1;
+            });
+
+            afficherFilmographie();
+        });
+    });
+};
+
+
+
+function afficherFilmographie(){
+
+    for (let i=0 ;i<=photoFilmographie.length-1 ;i++){
+
+        const adresseImage = 'https://image.tmdb.org/t/p/w500/' + filmographieActeur[idFilmographie+i].backdrop_path;
+        const titreFilm = filmographieActeur[idFilmographie+i].original_title + "_" + filmographieActeur[idFilmographie+i].release_date;
+    
+        document.getElementById('photoFilmographie'+i).src = adresseImage;
+        document.getElementById('titreFilmographie'+i).textContent = titreFilm;
+    }
+
+    ctner_infos_acteur.classList.remove('slide-out');
+    ctner_infos_acteur.classList.add('slide-in');
+    
+}
+
+btn_retour.addEventListener('click',()=>{
+    ctner_infos_acteur.classList.remove('slide-in');
+    ctner_infos_acteur.classList.add('slide-out');
+});
+
+
+
+// ALIMENTER RECHERCHE '______________________________________________________________________________________________________'
 
 fetch('https://api.themoviedb.org/3/movie/popular?api_key=b642a2df5b6d3048d4f07cd2a377518c&language=en-US&page=1')
 .then((response)=>{
-    console.log(response);
     response.json()
     .then((json)=>{
-        console.log(json);
-        console.log(json.total_pages);
         const nbPages = json.total_pages;
         alimenterListeFilms ();
     })
@@ -33,13 +144,10 @@ fetch('https://api.themoviedb.org/3/movie/popular?api_key=b642a2df5b6d3048d4f07c
 
 function alimenterListeFilms() {
     titresFilms = [];
-    console.log("________________________________________");
     une().then(deux);
 }
 
 function une() {
-
-    console.log("_1_");
 
     return new Promise((resolve, reject) => {
         let tableauDePromesses = [];
@@ -58,8 +166,6 @@ function une() {
 }
 
 function deux() {
-    console.log("_2_");
-    console.log(titresFilms);
     const listeFilms = document.getElementById('listeFilms');
     titresFilms.forEach(e => {
         const option = document.createElement('option');
@@ -68,221 +174,63 @@ function deux() {
     });
 }
 
-
-
-// function alimenterListeFilms(){
-
-//     titresFilms=[];
-
-//     console.log("DEBUT");
-//     une(deux);
-    
-
-// };
-
-
-//     function une(callback){
-//         for(let i=1;i<=10;i++){
-//             fetch('https://api.themoviedb.org/3/movie/popular?api_key=b642a2df5b6d3048d4f07cd2a377518c&language=en-US&page=' + i)
-//             .then((response)=>{
-//                 response.json()
-//                 .then((json)=>{
-//                     // console.log(json);
-//                     let films = json.results;
-//                     // console.log(films);
-                    
-//                     films.map((element,key,array)=>{
-//                         // console.log(element.original_title);
-                        
-//                         titresFilms.push(element.original_title);
-//                         // console.log(element.original_title);
-//                     })
-
-//                     // console.log(titresFilms);
-//                 });
-
-
-//             });
-//         };
-
-//         setTimeout(()=>{
-//             callback();
-//         },500)
-        
-//     }
-
-//     function deux(){
-
-//         console.log(titresFilms[0]);
-
-//         console.log(titresFilms);
-//         console.log(titresFilms[0]);
-        
-//         const listeFilms = document.getElementById('listeFilms');
-
-//         titresFilms.forEach((e)=>{
-//             const option = document.createElement('option');
-        
-//             option.value = e;
-//             listeFilms.appendChild(option);
-//         });
-
-//     }
-
-// -------------------------------------------------------------------------
-
-// boutonAfficher.addEventListener('click',()=>{
-//     alimenterListeFilms();
-//     // afficher(filmsActeur);
-// });
-
-boutonRetour.addEventListener('click',()=>{
-    masquer(filmsActeur);
-});
-
-function afficher(e){
-
-    if (e.classList.contains('slide-out')){
-        e.classList.remove('slide-out');
-    }
-
-    if (e.classList.contains('slide-in')){
-        e.classList.remove('animation-fade-in');
-        setTimeout(()=>{
-            e.classList.add('animation-fade-in');
-        },0)
-        
-    }else{
-        e.classList.add('slide-in');
-    }
-
-};
-
-function masquer(e){
-
-    if (e.classList.contains('slide-in')){
-        e.classList.remove('slide-in');
-    }
-    e.classList.add('slide-out');
-
-};
-
-
-function afficherFilmographie(){
-
-    for (let i=0 ;i<=11 ;i++){
-        const adresseImage = 'https://image.tmdb.org/t/p/w500/' + filmographieActeur[idFilmographie+i].backdrop_path;
-        const titreFilm = filmographieActeur[idFilmographie+i].original_title + "_" + filmographieActeur[idFilmographie+i].release_date;
-    
-        document.getElementById('photoFilmographie'+i).src = adresseImage;
-        document.getElementById('titreFilmographie'+i).textContent = titreFilm;
-    }
-
-    afficher(filmsActeur);
-    
-
-}
-
-boutonFilmographieSuivant.addEventListener("click", ()=>{
-    if(idFilmographie+22<=filmographieActeur.length){
-        const increment = 11;
-        idFilmographie=idFilmographie+increment;
-    }else{
-        // console.log("limite");
-        idFilmographie=filmographieActeur.length-12;
-    }
-
-    afficherFilmographie();
-    
-});
-
-// -------------------------------------------------------------------------
+// RECHERCHE FILM'______________________________________________________________________________________________________'
 
 
 btn_rechercher.addEventListener('click', ()=>{
-    const filmRecherche = document.getElementById('boiteDialogueFilmRecherche').value;
+    const filmRecherche = document.getElementById('input_recherche').value;
     requetebtn_rechercher(filmRecherche);
 });
 
-
-
-
-photoActeurs.forEach((e)=>{
-    e.addEventListener('click', ()=>{
-        const idActeur = Number(e.id.substring(e.id.length-1))+idCasting;
-        // console.log("idCasting : " + idCasting)
-        // console.log("idActeur : " + idActeur)
-        // console.log(e.id.substring(e.id.length-1,e.id.length));
-        // attention le substring c'est de tel caractère à tel caractère
-        // et non pas de tel caractère + le nombre de caractères souhaités
-        // attention une chaine = tableau donc commence à 0
-        // console.log(casting[idActeur].name);
-        // console.log(casting[idActeur]);
-        requeterInfosActeur(casting[idActeur].id);
-    })
-})
-
-function requeterInfosActeur(id){
-    idFilmographie=0;
-    fetch('https://api.themoviedb.org/3/person/' + id + '/movie_credits?api_key=b642a2df5b6d3048d4f07cd2a377518c&language=en-US')
-    .then((response)=>{
-        console.log(response)
+function requetebtn_rechercher(filmRecherche){
+    console.log ('Film recherché : ' + filmRecherche)
+    fetch('https://api.themoviedb.org/3/search/movie?api_key=b642a2df5b6d3048d4f07cd2a377518c&query=' + filmRecherche + '&include_adult=false&language=en-US&page=1')
+    .then ((response)=>{
         response.json()
         .then((json)=>{
-            console.log(json);
-            filmographieActeur=json.cast;
+            idFilm = json.results[0].id;
+            appelerRequetes();
+        })
+    })
 
-            filmographieActeur.sort(function trie(a,b){
-                // console.log(a.release_date);
-                // console.log(b.release_date);
 
-                if (a.release_date ==""){
-                    date1="1000-01-01"
-                }else{
-                    date1 = new Date(a.release_date)
-                }
-
-                if (b.release_date == ""){
-                    date2="1000-01-01"
-                }else{
-                    date2 = new Date(b.release_date)
-                }
-
-                // console.log("date1 : " + date1);
-                // console.log("date2 : " + date2);
-
-                return date2 - date1;
-            });
-
-            // console.log(filmographieActeur);
-            afficherFilmographie();
-        });
-    });
 };
 
-'______________________________________________________________________________________________________'
 
-btn_acteur_suivant.addEventListener('click',()=>{
-    idCasting++;
+
+// ACTEURS'______________________________________________________________________________________________________'
+
+btn_acteurs_suivants.addEventListener('click',()=>{
+    idCasting+=5;
+    
     ctrlIdCasting()
     afficherActeur(idCasting);
 });
 
-btn_acteur_precedent.addEventListener('click',()=>{
-    idCasting--;
+btn_acteurs_precedents.addEventListener('click',()=>{
+    idCasting-=5;
+
     ctrlIdCasting()
     afficherActeur(idCasting);
 
 });
 
 function ctrlIdCasting(){
-
-    if(idCasting>casting.length-photoActeurs.length){
-        idCasting=casting.length-photoActeurs.length;
+    if(idCasting+5>casting.length){
+        idCasting=casting.length-6;
     }else if(idCasting<0){
         idCasting=0;
     }
 }
+
+function afficherActeur(x){
+
+    for (let i=x;i<=x+photoActeurs.length-1;i++){
+        document.getElementById('photoActeur' +(i-x)).src = 'https://image.tmdb.org/t/p/w500/' + casting[i].profile_path;
+        document.getElementById('nomActeur' + (i-x)).textContent = casting[i].name;
+    };
+
+};
 
 function requeterInfosCasting(){
 
@@ -302,35 +250,22 @@ function requeterInfosCasting(){
     })
 };
 
+// FILM'______________________________________________________________________________________________________'
 
-
-function afficherActeur(x){
-
-    for (let i=x;i<=x+photoActeurs.length-1;i++){
-        document.getElementById('photoActeur' +(i-x + 1)).src = 'https://image.tmdb.org/t/p/w500/' + casting[i].profile_path;
-        document.getElementById('nomActeur' + (i-x + 1)).textContent = casting[i].name;
-    };
-
-};
-
-'______________________________________________________________________________________________________'
 
 function requeterInfosFilm(){
 
     fetch('https://api.themoviedb.org/3/movie/' + idFilm + '?api_key=b642a2df5b6d3048d4f07cd2a377518c')
     .then ((response)=>{
-        // console.log(response);
 
         if (response.status ==200){
 
             response.json()
             .then ((json)=>{
-                // console.log(json);
 
                 titreFilm.textContent=json.original_title;
 
                 const adresseImage1 = 'https://image.tmdb.org/t/p/w500/' + json.poster_path;
-                // console.log(adresseImage1);
                 document.getElementById('img_film_avant').src = adresseImage1;
 
                 const adresseImage2 = 'https://image.tmdb.org/t/p/w500/' + json.backdrop_path;
@@ -342,10 +277,6 @@ function requeterInfosFilm(){
                 document.getElementById('gains').innerText=json.revenue.toLocaleString();
                 document.getElementById('popularite').innerText=json.popularity;
                 document.getElementById('note').innerText=Math.round(json.vote_average*100)/100;
-                // var nombre = 5.56845;
-                // arrondi = nombre*100; → 556.845
-                // arrondi = Math.round(arrondi); → 556
-                // arrondi = arrondi/100; → 5.56
                 document.getElementById('nombreVotes').innerText=json.vote_count.toLocaleString();
                 const dateBonFormat = json.release_date.substring(8,10)+"/"+ json.release_date.substring(5,7)+"/"+ json.release_date.substring(0,4)
                 document.getElementById('dateSortie').innerText=dateBonFormat ;
@@ -356,7 +287,6 @@ function requeterInfosFilm(){
                 liste_genres.innerHTML=``;
 
                 tableauGenres.forEach(element => {
-                    // console.log(element.name);
                     genresFilm =document.querySelector("#liste_genres");
                     let li = document.createElement('li');
                     li.innerHTML = element.name;
@@ -374,26 +304,8 @@ function requeterInfosFilm(){
     });
 };
 
-function requetebtn_rechercher(filmRecherche){
-    // console.log ('Film recherché : ' + filmRecherche)
-    fetch('https://api.themoviedb.org/3/search/movie?api_key=b642a2df5b6d3048d4f07cd2a377518c&query=' + filmRecherche + '&include_adult=false&language=en-US&page=1')
-    .then ((response)=>{
-        // console.log(response);
-        response.json()
-        .then((json)=>{
-            // console.log(json);
-            idFilm = json.results[0].id;
-            appelerRequetes();
-        })
-    })
-
-
-};
 
 function appelerRequetes(){
-
-    // console.log("_______________________________________________");
-    console.log("Film ID : " + idFilm);
 
     requeterInfosFilm();
     requeterInfosCasting();
